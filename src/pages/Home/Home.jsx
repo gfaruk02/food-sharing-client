abe1dde75f6738c21486353f6c52a458d87ec0d5
gfaruk02 <gfaruk02@gmail.com>
2023-11-05 15:31:10 +0600

    design show all oods button and link to show all foods page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,13 +9,15 @@ const Home = () => {
     return (
         <div>
             <div>
-                <h1>Featured Foods</h1>
+                <h1 className="text-center text-5xl py-8 font-extrabold">Featured Foods</h1>
                 <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between gap-5">
-                {
-                    showDisplayFoods.map(food=> <FeaturedFoods key={food._id} food={food}></FeaturedFoods>)
-                }
+                    {
+                        showDisplayFoods.map(food => <FeaturedFoods key={food._id} food={food}></FeaturedFoods>)
+                    }
                 </div>
-                <button>Show All Foods</button>
+                <div className="text-center my-5">
+                    <button className="text-white font-bold text-2xl hover:bg-green-400 bg-green-800 py-4 px-8 rounded-lg"> Show All Foods </button>
+                </div>
             </div>
         </div>
     );
